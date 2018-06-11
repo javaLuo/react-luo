5e3a8916ca381166d2a877e7ef26f207d50fe69a
L <376693576@qq.com>
2018-06-11 12:37:09 +0800

    更新一点说明

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -10,9 +10,10 @@ const CopyWebpackPlugin = require("copy-webpack-plugin"); // 复制文件用
 
 /**
  * 基础路径
- * 比如我上传到自己的服务器填写的就是："https://isluo.com/work/pwa/"，
+ * 比如我上传到自己的服务器填写的是："/work/pwa/"，最终访问为"https://isluo.com/work/pwa/#/"
  * 根据你自己的需求填写
- * "/" 就是根路径，比如最终项目上线的地址为：https://isluo.com/， 那就可以直接写"/", 也可以写："https://isluo.com/"
+ * "/" 就是根路径，假如最终项目上线的地址为：https://isluo.com/， 那就可以直接写"/"
+ * 同时，public/manifest.json中的start_url也要和PUBLIC_PATH保持一致
  * **/
 const PUBLIC_PATH = "/";
 
