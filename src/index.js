16291ab00e70ed75aabee057d339bbc52a88532c
Logic <376693576@qq.com>
2019-07-22 15:19:02 +0800

    update npm

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 /** APP入口 **/
-import 'core-js/stable';
-import 'regenerator-runtime/runtime';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+import React from "react";
+import ReactDOM from "react-dom";
 
-import * as serviceWorker from './serviceWorker';
-import Root from './root';
+import * as serviceWorker from "./serviceWorker";
+import Root from "./root";
 
 /** 公共样式 **/
-import './styles/css.css';
-import './styles/less.less';
+import "./styles/css.css";
+import "./styles/less.less";
 
-ReactDOM.render(<Root />, document.getElementById('app-root'));
+ReactDOM.render(<Root />, document.getElementById("app-root"));
 
 serviceWorker.register();
 
